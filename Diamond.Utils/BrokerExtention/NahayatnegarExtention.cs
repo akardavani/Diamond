82736d87d8b0f3397898fa8c0f75641179ad937c
akardavani <akardavani@gmail.com>
2023-10-24 16:04:42 +0330

    Add files via upload

---
+++ Diamond.Utils/BrokerExtention/NahayatnegarExtention.cs
@@ -0,0 +1,35 @@
+﻿using Diamond.Domain.Enums;
+
+namespace Diamond.Utils.BrokerExtention
+{
+    public static class NahayatnegarExtention
+    {
+        public static string GetTimeFrame(TimeframeEnum timeFrame)
+        {
+            var result = "";
+            switch (timeFrame)
+            {
+                case TimeframeEnum.Weekly:
+                    result = "1W";
+                    break;
+                case TimeframeEnum.Daily:
+                    result = "1D";
+                    break;
+                case TimeframeEnum.FourHours:
+                    result = "4h";
+                    break;
+                case TimeframeEnum.OneHour:
+                    result = "1";
+                    break;
+                case TimeframeEnum.FifteenMinutes:
+                    result = "15m";
+                    break;
+                case TimeframeEnum.FiveMinutes:
+                    result = "5m";
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
